a0b245a1ba3f096bc5668899c1b84df00975889c
nicobrave <84059737+nicobrave@users.noreply.github.com>
2022-01-05 00:24:06 -0300

    cambios

---
+++ data/js/main.js
@@ -0,0 +1,11 @@
+var settings = {
+    "url": "https://nicobrave.github.io/data/test_data.json",
+    "method": "GET",
+    "timeout": 0,
+  };
+  
+  $.ajax(settings).done(function (data) {
+    console.log(data);
+  });
+
+ git commit
