fe3e3de45acd13fb5f356fc4ba533dc3a05e6d97
nicobrave <84059737+nicobrave@users.noreply.github.com>
2022-01-05 11:31:32 -0300

    ajustes menores

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
 var settings = {
-    "url": "https://nicobrave.github.io/data/test_data.json",
+    "url": "https://nicobrave.github.io/assets/data/test_data.json",
     "method": "GET",
     "timeout": 0,
   };
   
-  $.ajax(settings).done(function (id) {
-    console.log(id);
+  $.ajax(settings).done(function (data) {
+
+   let name = data.name
+
+    console.log(data);
   });
+
